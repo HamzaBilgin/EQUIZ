e80710c99a2feb6fbeb9fb9443a4e6ead8bc6d94
Hamza Bilgin <112509615+HamzaBilgin@users.noreply.github.com>
2024-03-17 17:14:31 +0300

    updated

--- src/components/Instructor/Instructor.jsx
+++ src/components/Instructor/Instructor.jsx
@@ -5,13 +5,19 @@ const Instructor = () => {
   return (
     <div className="flex flex-row justify-center items-center h-full w-full">
       <Link
-        to={`/instructor/quiz`}
+        to="/instructor/quiz"
         relative="path"
         className="p-5 border rounded-full cursor-pointer m-2"
       >
         <span>QUIZ</span>
       </Link>
-      <div className="p-5 border rounded-full cursor-pointer">RESULT</div>
+      <Link
+        to="/instructor/result"
+        relative="path"
+        className="p-5 border rounded-full cursor-pointer m-2"
+      >
+        <span>RESULT</span>
+      </Link>
     </div>
   );
 };
